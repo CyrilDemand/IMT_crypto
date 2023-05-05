28207b0585ec2f1c99f70b3928963a98af93c3ee
Noe-Delcroix <noe.delcroix59@gmail.com>
2023-05-05 14:33:32 +0200

    fix config

--- app/truffle/truffle-config.js
+++ app/truffle/truffle-config.js
@@ -38,7 +38,7 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "../client/src/contracts"),
   networks: {
     development: {
       host: "127.0.0.1", // Localhost (default: none)
