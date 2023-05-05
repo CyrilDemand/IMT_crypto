28bb63364dd3a369b4aa16837d4e4c58887c4999
Noe-Delcroix <noe.delcroix59@gmail.com>
2023-05-05 09:23:40 +0200

    config contract blockchain pas locale

--- app/truffle/migrations/2_deploy_voting.js
+++ app/truffle/migrations/2_deploy_voting.js
@@ -1,4 +1,4 @@
-const SimpleStorage = artifacts.require("Voting");
+const Voting = artifacts.require("Voting");
 
 module.exports = function (deployer) {
   deployer.deploy(Voting);
--- app/truffle/truffle-config.js
+++ app/truffle/truffle-config.js
@@ -42,17 +42,17 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Standard Ethereum port (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
-    ropsten: {
+    sepolia: {
       provider: function () {
         return new HDWalletProvider({
           mnemonic: { phrase: `${process.env.MNEMONIC}` },
-          providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`,
+          providerOrUrl: `https://sepolia.infura.io/v3/${process.env.INFURA_ID}`,
         })
       },
-      network_id: 3,
+      network_id: "*",
     },
     kovan: {
       provider: function () {
@@ -109,7 +109,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13", // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.18", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
